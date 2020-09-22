e4969be691b8fd054ff7a7ddc357526fdd9c4aca
Mohammed Al-Thawr <malthawr@gmail.com>
2020-09-22 16:19:50 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 import Form from './components/Form'
 
 ReactDOM.render(
+  <div>import React from 'react';
+import ReactDOM from 'react-dom';
+import ParentComponent from './components/ParentComponent';
+
+ReactDOM.render(
   <div>
+    <ParentComponent/>
+  </div>,
+  document.getElementById('root')
+);
     <Form />
   </div>,
   document.getElementById('root')
